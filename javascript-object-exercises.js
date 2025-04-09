c21fa177b40f1c0bdd1f72298bad44c358f4ee94
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-09 13:56:34 +0700

    feat(object): add new student object

--- javascript-object-exercises.js
+++ javascript-object-exercises.js
@@ -11,3 +11,18 @@ console.log(person["job"])
 
 // ## 3. Add New Property
 Object.defineProperty(person,"hobby", {value: "Reading"})
+
+// ## 4. Update Property
+Object.defineProperty(person, "age", {value: 30})
+
+// ## 5. Delete Property
+delete person.job;
+
+// ## 6. Object Inside Object
+const student = {
+    name : "Alice",
+    grade: 90,
+    address : {city: "Jakarta", country:"Indonesia"}
+}
+
+console.log(student.address.city)
