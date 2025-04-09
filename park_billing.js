4a370b9a8c8e706b1674e99d7de9a69563123473
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-09 16:55:02 +0700

    feat(object): add park billing logic

---
+++ park_billing.js
@@ -0,0 +1,18 @@
+function billing(veh, dur){
+    dur = Math.ceil(dur)
+    if (veh== 'car'){
+        if (dur > 24){
+            return 5000 + (3000*(dur-1)) + 50000
+        }
+        return 5000 + (3000*(dur-1))
+    }
+    if (dur > 24){
+        return 2000 + (1000*(dur-1)) + 20000
+    }
+    return 2000 + (1000*(dur-1))
+}
+
+console.log(billing('Car',3))
+console.log(billing('bike',1))
+console.log(billing('Car',27))
+console.log(billing('bike',27))
